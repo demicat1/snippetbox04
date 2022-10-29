16c90d0f21450efe4e60c9b8a0570ce8bc7c607e
Adilkhan <adilkhant19@gmail.com>
2022-10-29 15:58:52 +0600

    4 chapter

---
+++ cmd/web/handlers.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"net/http"
+	"strconv"
+
+	"com.snippetbox04.aitu/internal/models"
+)
+
+func (app *application) home(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		app.notFound(w)
+		return
+	}
+
+	snippets, err := app.snippets.Latest()
+
+	if err != nil {
+		app.serverError(w, err)
+		return
+	}
+
+	for _, snippet := range snippets {
+		fmt.Fprintf(w, "%+v\n", snippet)
+	}
+}
+
+func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	if err != nil || id < 1 {
+		app.notFound(w)
+		return
+	}
+
+	snippet, err := app.snippets.Get(id)
+	if err != nil {
+		if errors.Is(err, models.ErrNoRecord) {
+			app.notFound(w)
+		} else {
+			app.serverError(w, err)
+		}
+	}
+
+	fmt.Fprintf(w, "%+v", snippet)
+}
+
+func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	if r.Method != http.MethodPost {
+		w.Header().Set("Allow", http.MethodPost)
+		app.clientError(w, http.StatusMethodNotAllowed)
+		return
+	}
+
+	title := "O snail"
+	content := "O snail'\n'Climb Mount Fuji,'\n'But slowly, slowly!'\n''\n'– Kobayashi Issa"
+	expires := 7
+
+	id, err := app.snippets.Insert(title, content, expires)
+
+	if err != nil {
+		app.serverError(w, err)
+		return
+	}
+
+	http.Redirect(w, r, fmt.Sprintf("/snippet/view?id=%d", id), http.StatusSeeOther)
+
+	//w.Write([]byte("Creating a new adopting request..."))
+}
---
+++ cmd/web/main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"log"
+	"net/http"
+	"os"
+
+	"com.snippetbox04.aitu/internal/models"
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+type application struct {
+	infoLog  *log.Logger
+	errorLog *log.Logger
+	snippets *models.SnippetModel
+}
+
+func main() {
+	//terraria
+	addr := flag.String("addr", ":4000", "HTTP network address")
+	dsn := flag.String("dsn", "postgresql://web:pass@localhost:5432/snippetbox", "PostgreSQL data source name")
+	flag.Parse()
+
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+	db, err := openDB(*dsn)
+	if err != nil {
+		errorLog.Fatal(err)
+	}
+
+	defer db.Close()
+
+	app := &application{
+		errorLog: errorLog,
+		infoLog:  infoLog,
+		snippets: &models.SnippetModel{DB: db},
+	}
+
+	srv := &http.Server{
+		Addr:     "localhost" + *addr,
+		ErrorLog: errorLog,
+		Handler:  app.routes(),
+	}
+
+	infoLog.Printf("Starting server on: %s", *addr)
+	err = srv.ListenAndServe()
+	errorLog.Fatal(err)
+}
+
+func openDB(dsn string) (*pgxpool.Pool, error) {
+	ctx := context.Background()
+	db, err := pgxpool.New(ctx, dsn)
+	if err != nil {
+		return nil, err
+	}
+	if err = db.Ping(ctx); err != nil {
+		return nil, err
+	}
+	return db, nil
+}
